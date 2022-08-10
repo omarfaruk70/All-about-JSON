83adba2428ec5cb44c95fb522d8fa21ad68b292a
omarfaruk70 <faruk1611281892@gmail.com>
2022-08-11 01:10:09 +0600

    initial commit

---
+++ class 01/script.js
@@ -0,0 +1,24 @@
+//  JSON is the short form of javascript object Notation.
+//  json is not a programming language. it is a human readable, text based data format which is globally used.
+// any programmer can be used json data.
+//  json is a  global text format(string) which can be used any programming language
+//  like js, php, python, c , c++, c# etc.
+
+
+// JSON is not dependable any programming languages . But all other programming languages
+// used json for data access.
+// JSON always followed the syntex of javascript.
+// JSON is like a javascript Object . But its totally different from a js Object.
+//  JSON is wrapped with ("").
+
+
+// ** the core concept of json is used to store & exchange of data between client and server.**
+// json file extention is (.json)
+// we need to sharp knowledge about Javascript Object and array to explore JSON. cause json work with Object and array.
+//  we can see a basic json format is the below
+{
+    'name' : "Omar Faruk",
+    'age' : 20,
+    "isStudent" : true,
+    'goal' : "A fullstack web developer"    
+}
