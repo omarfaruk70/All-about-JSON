a325ee3cd9b7a00f668069efb6b42097bcd8bb41
omarfaruk70 <faruk1611281892@gmail.com>
2022-08-11 10:55:59 +0600

    json class 04 done

---
+++ class 04/script.js
@@ -0,0 +1,4 @@
+// JSON warning
+// javascript do not suppot json formate. first we should convert
+// it as a json formate by using JSON.stringify() method . 
+// and then we can convert it object method by JSON.parse() methods.
