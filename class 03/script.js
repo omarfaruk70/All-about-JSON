53342826ab2d2613dfa88369bdc2964cf8ccbb27
omarfaruk70 <faruk1611281892@gmail.com>
2022-08-11 10:38:16 +0600

    class 03 done

---
+++ class 03/script.js
@@ -0,0 +1,20 @@
+// JSON support those data types of javascript. and they are...
+// 1. string.
+// 2. number. 
+// 3. boolean.
+// 4. array. 
+// 5. Object. (JSON formate)
+// 6. null. 
+// JSON official website is   www.jsonlint.com
+let student = {
+    name : 'rahim',
+    age : 20,
+    isStudent : true,
+    havingGF : null,
+    greet: function(){
+        console.log('welcome to json class')
+    }
+}
+let ans = JSON.stringify(student)
+console.log(ans)
+// JSON will not support undifined , function, date type data .
